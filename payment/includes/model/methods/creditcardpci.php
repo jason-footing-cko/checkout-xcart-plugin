37f6dbaaa6749ccb3f68c60187ac9d791011f5ca
jason-footing-cko <jason.footing@checkout.com>
2015-05-13 10:32:42 +0400

    xcart integration fix

--- payment/includes/model/methods/creditcardpci.php
+++ payment/includes/model/methods/creditcardpci.php
@@ -4,16 +4,12 @@ class model_methods_creditcardpci extends model_methods_Abstract
 {
     public function handleRequest()
     {
-        global $userinfo;
-
         $config = parent::handleRequest();
-        $config['postedParam']['card']['phoneNumber'] = $userinfo['b_phone'];
         $config['postedParam']['card']['name'] = $_POST['card_name'];
         $config['postedParam']['card']['number'] = (string)$_POST['card_no'];
         $config['postedParam']['card']['expiryMonth'] = $_POST['card_expdate_Month'];
         $config['postedParam']['card']['expiryYear'] = $_POST['card_expdate_Year'];
         $config['postedParam']['card']['cvv'] = $_POST['card_cvv'];
-
         $this->_placeorder($config);
     }
 
